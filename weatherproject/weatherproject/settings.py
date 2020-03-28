feecd6d7b4ce1ef76a5e3c1820f066d7cdf1fe97
ROroutsi <31228382+ROroutsi@users.noreply.github.com>
2020-03-28 03:23:40 +0000

    Update settings.py

--- weatherproject/weatherproject/settings.py
+++ weatherproject/weatherproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nx=!_h89&smla&el34$%n#z1xl1&pn1exe)v8yxy7wc1@9x6(9'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
